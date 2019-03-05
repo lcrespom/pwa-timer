208b7113345aefabb33e44c076142fc221cb638c
Luis Crespo <lcrespom@gmail.com>
2019-03-05 09:54:59 +0100

    Proper tracing

--- web/sw.js
+++ web/sw.js
@@ -1,28 +1,33 @@
-console.log('Hello from SW')
+console.log("Hello from SW")
 
 self.onmessage = msg => {
 	switch (msg.data.command) {
-		case 'timer-start': return startTimer(msg.data.params)
-		default: throw Error('Unknown command: ' + msg.data.command)
+		case "timer-start": return startTimer(msg.data.params)
+		default: throw Error("Unknown command: " + msg.data.command)
 	}
 }
 
 function startTimer(params) {
-    domlog('SW: start timer', params)
-    setTimeout(() => {
-        timerNotification(params.time)
-        domlog('SW: end timer')
-        // @ts-ignore
-        postMessage({ command: 'timer-end', params })
-    }, params.time * 1000)
+	domlog("SW: start timer", params)
+	domlog("Notification.permission:", Notification.permission)
+	setTimeout(() => {
+		try {
+			timerNotification(params.time)
+			domlog("SW: end timer")
+			// @ts-ignore
+			postMessage({ command: "timer-end", params })
+		} catch (e) {
+			domlog("Exception inside setTimeout:", e.message)
+		}
+	}, params.time * 1000)
 }
 
 function timerNotification(time) {
-    if (Notification.permission != 'granted') return
-    new Notification('Timer: ' + time)
+	if (Notification.permission != "granted") return
+	new Notification("Timer: " + time)
 }
 
 function domlog(...params) {
-    // @ts-ignore
-    postMessage({ command: 'dom-log', params })
+	// @ts-ignore
+	postMessage({ command: "dom-log", params })
 }
