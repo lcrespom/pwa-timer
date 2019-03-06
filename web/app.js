a9bcfa452f2ee8f16b261ee94986f9a1e6c585bc
Luis Crespo <lcrespom@gmail.com>
2019-03-06 09:24:20 +0100

    Using ServiceWorkerRegistration

--- web/app.js
+++ web/app.js
@@ -1,20 +1,21 @@
-// @ts-ignore
+// @ts-nocheck
 $(function() {
     navigator.serviceWorker.register('sw.js');
     let worker = new Worker('worker.js')
 
-    // @ts-ignore
     $('#go_timer').click(_ => {
-        // @ts-ignore
         let time = $('#time').val()
         worker.postMessage({ command: 'timer-start', params: { time } })
     })
+    $('#go_test').click(_ => showNotification('Test notification'))
 
     worker.onmessage = msg => {
+        let params = msg.data.params
 		switch (msg.data.command) {
             case 'timer-end': return domlog('Timer end')
-            case 'dom-log': return domlog(...msg.data.params)
-			default: throw Error('Unknown command: ' + msg.data.command)
+            case 'dom-log': return domlog(...params)
+            case 'notify': return showNotification(params.title, params.options)
+			default: throw Error('Unknown command: ' + command)
 		}
     }
 
@@ -27,7 +28,18 @@ $(function() {
                 ? param
                 : JSON.stringify(param)
         )
-        // @ts-ignore
         $('#logarea').append(params.join(' ') + '\n')
+    }
+
+    function showNotification(msg, options) {
+        domlog('Notification:', msg, options)
+        try {
+            navigator.serviceWorker.ready.then(registration => {
+                registration.showNotification(msg, options)
+                domlog('Notification sent')
+            })
+        } catch (e) {
+            domlog('showNotification error:', e.message)
+        }
     }
 })
--- web/worker.js
+++ web/worker.js
@@ -1,35 +1,33 @@
-console.log("Hello from worker")
+// @ts-nocheck
+console.log('Hello from worker')
 
 self.onmessage = msg => {
 	switch (msg.data.command) {
-		case "timer-start": return startTimer(msg.data.params)
-		default: throw Error("Unknown command: " + msg.data.command)
+		case 'timer-start': return startTimer(msg.data.params)
+		default: throw Error('Unknown command: ' + msg.data.command)
 	}
 }
 
 function startTimer(params) {
-	domlog("W: start timer", params)
-	domlog("W: Notification.permission:", Notification.permission)
+	domlog('W: start timer', params)
+	domlog('W: Notification.permission:', Notification.permission)
 	setTimeout(() => {
 		try {
 			timerNotification(params.time)
-			domlog("W: end timer")
-			// @ts-ignore
-			postMessage({ command: "timer-end", params })
+			domlog('W: end timer')
+			postMessage({ command: 'timer-end', params })
 		} catch (e) {
-			domlog("Exception inside setTimeout:", e.message)
+			domlog('Exception inside setTimeout:', e.message)
 		}
 	}, params.time * 1000)
 }
 
 function timerNotification(time) {
-	if (Notification.permission != "granted") return
-	navigator.serviceWorker.ready.then(registration =>
-		registration.showNotification('Timer: ' + time)
-	)
+	if (Notification.permission != 'granted') return
+	let title = 'Timer: ' + time
+	postMessage({ command: 'notify', params: { title }})
 }
 
 function domlog(...params) {
-	// @ts-ignore
-	postMessage({ command: "dom-log", params })
+	postMessage({ command: 'dom-log', params })
 }
